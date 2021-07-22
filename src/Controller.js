773439f1e7ae25747dcb2f2ce9f9ca414cf9796f
SaranyaSivadasan <68867201+SaranyaSivadasan@users.noreply.github.com>
2021-07-23 00:10:25 +0530

    Added DashboardLayout

---
+++ src/Controller.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import DashboardLayout from './components/dashboardLayout/Dashboard';
+
+const Controller = () => {
+    return(
+        <Router>
+            <DashboardLayout>
+                this is DashboardLayout
+            <Switch>
+                <Route />
+            </Switch>
+            </DashboardLayout>
+            
+        </Router>
+    )
+}
+
+export default Controller;
